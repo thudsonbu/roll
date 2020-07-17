146ce050897acfcf353683d3ea3e572339f502ad
thudsonbu <hudson_thom@bentley.edu>
2020-07-17 09:01:21 -0600

    Styling Lotto

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,7 @@ class Die extends Component {
     render() {
         return (
             <div>
-                 <i className={`Die fas fa-dice-${this.props.face} 
+                <i className={`Die fas fa-dice-${this.props.face} 
                         ${this.props.rolling ? "rolling" : ""}
                     `}
                 />
